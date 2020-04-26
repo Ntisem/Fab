825bee2230eb0c2060904f702d43165a46476503
William Ntisem <williamntisem123@gmail.com>
2020-04-26 22:20:34 +0000

    New files

---
+++ assets/js/data_submit.js
@@ -0,0 +1,45 @@
+$(document).ready(()=>{
+
+    var cloudUrl = 'https://api.cloudinary.com/v1_1/rosefabricsgh/upload'
+    var uploadPreset = 'fabricFinder'
+    var file=null
+
+    $('#fabric_image').on('change',e=>{
+        file = e.target.files[0]
+    })
+
+    $('#data-info').click(function(e){
+        e.preventDefault();
+        var formData = new FormData()
+        formData.append('file',file)
+        formData.append('upload_preset',uploadPreset)
+
+        axios({
+            url:cloudUrl,
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/x-www-form-urlencoded',
+            },
+            data:formData
+        }).then(res=>{
+            var imageUrl = res.data.secure_url
+            console.log('Image Submitted')
+            axios({
+                method: 'post',
+                url: 'https://cors-anywhere.herokuapp.com/https://rose-pay.herokuapp.com/finder/',
+                data: {
+                    fabric_image: imageUrl,
+                    description: $("#description").val(),
+                    phone_number: $("#phone_number").val()
+                }
+            }).then(function(response) {
+                $('#thankyou').appendTo("body").modal(); 
+                console.log('data submitted')
+            })
+            // $("#closer").click(function () {
+            //     $("#thankyou").dialog("close");
+            // });
+        })
+        .catch(err=>console.log(err))
+      });
+})
